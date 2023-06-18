70415b9c32dddf05ca366f83f6d6f6008380414c
Ankit Dash <ankitdash2019@gmail.com>
2023-06-18 12:14:30 +0530

    added functionality on on-screen keyboard

--- client/src/components/keyboard.jsx
+++ client/src/components/keyboard.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 
 const Keyboard = () => {
+  const handleKeyPress = (key) => {
+    const event = new KeyboardEvent('keydown', { key }); // it is a javascript API
+    document.dispatchEvent(event);
+  };
+
   return (
     <div id="keyboard">
       <div className="first-row">
-        <button className="keyboard-button">Q</button>
-        <button className="keyboard-button">W</button>
-        <button className="keyboard-button">E</button>
-        <button className="keyboard-button">R</button>
-        <button className="keyboard-button">T</button>
-        <button className="keyboard-button">Y</button>
-        <button className="keyboard-button">U</button>
-        <button className="keyboard-button">I</button>
-        <button className="keyboard-button">O</button>
-        <button className="keyboard-button">P</button>
-        <button className="keyboard-button">Backspace</button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('Q')}> Q </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('W')}> W </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('E')}> E </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('R')}> R </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('T')}> T </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('Y')}> Y </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('U')}> U </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('I')}> I </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('O')}> O </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('P')}> P </button>
+        <button className="keyboard-button" onClick={() => handleBackspace()}> Backspace </button>
       </div>
       <div className="second-row">
-        <button className="keyboard-button">A</button>
-        <button className="keyboard-button">S</button>
-        <button className="keyboard-button">D</button>
-        <button className="keyboard-button">F</button>
-        <button className="keyboard-button">G</button>
-        <button className="keyboard-button">H</button>
-        <button className="keyboard-button">J</button>
-        <button className="keyboard-button">K</button>
-        <button className="keyboard-button">L</button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('A')}> A </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('S')}> S </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('D')}> D </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('F')}> F </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('G')}> G </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('H')}> H </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('J')}> J </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('K')}> K </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('L')}> L </button>
       </div>
       <div className="third-row">
-        <button className="keyboard-button">Delete</button>
-        <button className="keyboard-button">Z</button>
-        <button className="keyboard-button">X</button>
-        <button className="keyboard-button">C</button>
-        <button className="keyboard-button">V</button>
-        <button className="keyboard-button">B</button>
-        <button className="keyboard-button">N</button>
-        <button className="keyboard-button">M</button>
-        <button className="keyboard-button">Enter</button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('Delete')}> Delete </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('Z')}> Z </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('X')}> X </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('C')}> C </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('V')}> V </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('B')}> B </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('N')}> N </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('M')}> M </button>
+        <button className="keyboard-button" onClick={() => handleKeyPress('Enter')}> Enter </button>
       </div>
     </div>
   );
