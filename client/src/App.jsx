21c41b6684c227be01070688cb39ee98a6d538a7
Devaansh-Kumar <devaanshk840@gmail.com>
2023-06-19 21:54:30 +0300

    created single player wordle component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Letter_Box } from './components/Letterbox';
 import GridComponent from './components/Grid'
 import Keyboard from './components/Keyboard'
 import './App.css'
+import WordleGame from './components/WordleGame';
 
 function App() {
   useEffect(() => {
@@ -13,8 +14,9 @@ function App() {
     <>
     <h1 className='app-name'>WORD-RUSH</h1>
       <div>
-        <GridComponent />
-        <Keyboard />
+        {/* <GridComponent />
+        <Keyboard /> */}
+        <WordleGame/>
       </div>
     </>
   )
